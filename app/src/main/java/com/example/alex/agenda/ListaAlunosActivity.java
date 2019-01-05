2b5f20c122aaae3d8641b0d45671d0bf2b2f788c
alex <alex.co.alves@gmail.com>
2019-01-04 22:56:44 -0200

    Abrindo navegador externo

--- app/src/main/java/com/example/alex/agenda/ListaAlunosActivity.java
+++ app/src/main/java/com/example/alex/agenda/ListaAlunosActivity.java
@@ -1,7 +1,9 @@
 package com.example.alex.agenda;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Browser;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.ContextMenu;
@@ -60,15 +62,13 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, final ContextMenu.ContextMenuInfo menuInfo) {
-        MenuItem deletar = menu.add("Deletar");
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+        final Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(info.position);
 
+        MenuItem deletar = menu.add("Deletar");
         deletar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
-                AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
-
-                Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(info.position);
-
                 AlunoDAO dao = new AlunoDAO(ListaAlunosActivity.this);
                 dao.deleta(aluno);
                 dao.close();
@@ -78,6 +78,18 @@ public class ListaAlunosActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+        MenuItem visitarSite = menu.add("Visitar Site");
+        Intent intentSite = new Intent(Intent.ACTION_VIEW);
+
+        String site = aluno.getSite();
+        if(!site.startsWith("http")){
+            site = "http://" + site;
+        }
+
+        intentSite.setData(Uri.parse(aluno.getSite()));
+        visitarSite.setIntent(intentSite);
+
     }
 
     private void carregaLista() {
