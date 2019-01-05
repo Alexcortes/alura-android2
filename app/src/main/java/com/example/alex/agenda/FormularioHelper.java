d280f45950d8cb877855381224543afe58fe0628
alex <alex.co.alves@gmail.com>
2019-01-04 22:34:20 -0200

    Projeto inicial (Feito no curso Android 1)

---
+++ app/src/main/java/com/example/alex/agenda/FormularioHelper.java
@@ -0,0 +1,51 @@
+package com.example.alex.agenda;
+
+import android.widget.EditText;
+import android.widget.RatingBar;
+
+import com.example.alex.agenda.modelo.Aluno;
+
+public class FormularioHelper {
+
+    private EditText campoNome;
+    private EditText campoEndereco;
+    private EditText campoTelefone;
+    private EditText campoSite;
+    private RatingBar campoNota;
+
+    private Aluno aluno;
+
+
+    public FormularioHelper(FormularioActivity activity){
+        campoNome = activity.findViewById(R.id.formulario_nome);
+
+        campoEndereco = activity.findViewById(R.id.formulario_endereco);
+
+        campoTelefone = activity.findViewById(R.id.formulario_telefone);
+
+        campoSite = activity.findViewById(R.id.formulario_site);
+
+        campoNota = activity.findViewById(R.id.formulario_nota);
+
+        aluno = new Aluno();
+    }
+
+    public Aluno pegaAluno() {
+        aluno.setNome(campoNome.getText().toString());
+        aluno.setEndereco(campoEndereco.getText().toString());
+        aluno.setSite(campoSite.getText().toString());
+        aluno.setTelefone(campoTelefone.getText().toString());
+        aluno.setNota(Double.valueOf(campoNota.getProgress()));
+
+        return aluno;
+    }
+
+    public void preencheFormulario(Aluno aluno) {
+        campoNome.setText(aluno.getNome());
+        campoEndereco.setText(aluno.getEndereco());
+        campoSite.setText(aluno.getSite());
+        campoTelefone.setText(aluno.getTelefone());
+        campoNota.setProgress(aluno.getNota().intValue());
+        this.aluno = aluno;
+    }
+}
---
+++ app/src/main/java/com/example/alex/agenda/ListaAlunosActivity.java
@@ -0,0 +1,92 @@
+package com.example.alex.agenda;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.alex.agenda.dao.AlunoDAO;
+import com.example.alex.agenda.modelo.Aluno;
+
+import java.util.List;
+
+public class ListaAlunosActivity extends AppCompatActivity {
+    ListView listaAlunos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_alunos);
+
+        listaAlunos = findViewById(R.id.lista_alunos);
+
+        listaAlunos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> lista, View item, int position, long id) {
+                Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(position);
+                Intent intentVaiProFormulario = new Intent(ListaAlunosActivity.this, FormularioActivity.class);
+                intentVaiProFormulario.putExtra("Aluno", aluno);
+                startActivity(intentVaiProFormulario);
+            }
+        });
+
+        FloatingActionButton novoAluno = findViewById(R.id.lista_aluno_add);
+
+        novoAluno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent vaiProFormulario = new Intent(ListaAlunosActivity.this, FormularioActivity.class);
+                startActivity(vaiProFormulario);
+            }
+        });
+
+        registerForContextMenu(listaAlunos);
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        carregaLista();
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, final ContextMenu.ContextMenuInfo menuInfo) {
+        MenuItem deletar = menu.add("Deletar");
+
+        deletar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+
+                Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(info.position);
+
+                AlunoDAO dao = new AlunoDAO(ListaAlunosActivity.this);
+                dao.deleta(aluno);
+                dao.close();
+
+                carregaLista();
+
+                return false;
+            }
+        });
+    }
+
+    private void carregaLista() {
+        AlunoDAO dao =  new AlunoDAO(this);
+        List<Aluno> alunos = dao.buscaAlunos();
+        dao.close();
+
+        listaAlunos = findViewById(R.id.lista_alunos);
+        ArrayAdapter<Aluno> adapter = new ArrayAdapter<Aluno>(this, android.R.layout.simple_list_item_1, alunos);
+        listaAlunos.setAdapter(adapter);
+    }
+}
