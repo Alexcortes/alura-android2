c95856adcdbb1ca265ed041f2cd0e390fa06abc6
alex <alex.co.alves@gmail.com>
2019-01-04 23:22:00 -0200

    Realizando ligação

--- app/src/main/java/com/example/alex/agenda/ListaAlunosActivity.java
+++ app/src/main/java/com/example/alex/agenda/ListaAlunosActivity.java
@@ -1,10 +1,14 @@
 package com.example.alex.agenda;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Browser;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.ContextMenu;
 import android.view.MenuItem;
@@ -81,29 +85,38 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
         MenuItem visitarSite = menu.add("Visitar Site");
         Intent intentSite = new Intent(Intent.ACTION_VIEW);
-
         String site = aluno.getSite();
         if(!site.startsWith("http")){
             site = "http://" + site;
         }
-
         intentSite.setData(Uri.parse(aluno.getSite()));
         visitarSite.setIntent(intentSite);
 
 
         MenuItem enviarSms = menu.add("Enviar SMS");
         Intent intentEnviar = new Intent(Intent.ACTION_VIEW);
-
         intentEnviar.setData(Uri.parse("sms:"+aluno.getTelefone()));
         enviarSms.setIntent(intentEnviar);
 
         MenuItem itemMapa = menu.add("Visualizar no mapa");
         Intent intentMapa = new Intent(Intent.ACTION_VIEW);
-
         intentMapa.setData(Uri.parse("geo:0,0?q="+aluno.getEndereco()));
         itemMapa.setIntent(intentMapa);
 
-
+        MenuItem itemLigar = menu.add("Ligar");
+        itemLigar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if(ActivityCompat.checkSelfPermission(ListaAlunosActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(ListaAlunosActivity.this, new String[]{Manifest.permission.CALL_PHONE}, 123);
+                }else{
+                    Intent intentLigar = new Intent(Intent.ACTION_CALL);
+                    intentLigar.setData(Uri.parse("tel:"+aluno.getTelefone()));
+                    startActivity(intentLigar);
+                }
+                return false;
+            }
+        });
     }
 
     private void carregaLista() {
